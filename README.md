3d5dd0dd3350efd5c6b85630c11a921ebf1d9edd
AlexMcC02 <alex.mccullough@kainos.com>
2023-08-25 14:35:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Hecto Tutorial
+Following a great tutorial: https://www.flenker.blog/hecto/
+
+#### Progress Tracker: About to Start the Status Bar (Ch. 4)
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod editor;
+mod terminal;
+mod document;
+mod row;
+
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
+pub use document::Document;
+pub use row::Row;
+
+fn main() {
+    Editor::default().run()
+}
