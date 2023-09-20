6471548ef24d35ef6d7ce00b121b1062b2be4c17
AlexMcC02 <alex.mccullough@kainos.com>
2023-09-20 12:42:11 +0100

    feat: coloured strings

--- src/highlighting.rs
+++ src/highlighting.rs
@@ -5,13 +5,15 @@ pub enum Type {
     None,
     Number,
     Match,
+    String,
 }
 
 impl Type {
     pub fn to_color(&self) -> impl color::Color {
         match self {
             Type::Number => color::Rgb(220, 163, 163),
-            Type::Match => color::Rgb(38, 139, 210),
+            Type::Match => color::Rgb(38, 139, 210),    
+            Type::String => color::Rgb(211, 54, 130),
             _ => color::Rgb(255, 255, 255),
         }
     }
