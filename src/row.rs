2331c91aae99e4269c4f48194654882751dcc8ea
AlexMcC02 <alex.mccullough@kainos.com>
2023-09-26 09:50:50 +0100

    fix: small bugfix in row.rs

--- src/row.rs
+++ src/row.rs
@@ -180,7 +180,7 @@ impl Row {
                 if let Some(next_index) = search_match.checked_add(word[..].graphemes(true).count())
                 {
                     #[allow(clippy::indexing_slicing)]
-                    for i in index.saturating_add(search_match)..next_index {
+                    for i in search_match..next_index {
                         self.highlighting[i] = highlighting::Type::Match;
                     }
                     index = next_index;
